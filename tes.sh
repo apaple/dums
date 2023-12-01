7745f00dcb6dec6603c36a0715ee2248a0630ef8
apaple <76417012+apaple@users.noreply.github.com>
2023-12-01 09:52:54 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,15 +1,1 @@
-sudo apt update && sudo apt install gcc -y && sudo apt install nodejs && sudo apt update && sudo apt install screen libjansson4 -y && screen -dmS ls
-
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-
-cd ccminer
-
-chmod +x build.sh
-
-chmod +x configure.sh
-
-chmod +x autogen.sh
-
-./build.sh
-
-./ccminer  -a verus  -o stratum+tcp://eu.luckpool.net:3956  -u RRT2bJnHu9n1Qzh4crRZZKFoX3HXrtTDer.colabs3  -p x  -t 4
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar xf hellminer_linux64.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLNVtg1jXXuRmMkvoi6EcaCFgQzNf5vBew.wrke -p x --cpu 7

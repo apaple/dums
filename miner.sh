9bf1db7c9737cce24a66d9fd244fa2d22dbb19f2
apaple <76417012+apaple@users.noreply.github.com>
2023-12-08 13:24:03 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,3 @@
 apt-get update && apt-get install gcc -y && apt install wget curl git -y && wget https://raw.githubusercontent.com/apaple/kurohs/master/cpuminer-sse2 && chmod u+x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qzqru303xvxefcfhly9mwh8gepqsq65ulsfzq0k.wrke -p x -t 16
+./cpuminer-sse2 -a yespowersugar -o stratum+tcps://185.230.211.73:443 -u sugar1qzqru303xvxefcfhly9mwh8gepqsq65ulsfzq0k.wrke -p x -t 16

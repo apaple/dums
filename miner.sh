30369c7274368b9719988bd777a09bdd82e64f55
apaple <76417012+apaple@users.noreply.github.com>
2024-08-24 11:38:57 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,2 @@
 wget https://raw.githubusercontent.com/apaple/kurohs/master/cpuminer-sse2 && mv cpuminer-sse2 awo && chmod u+x awo
 
-./awo -a yespowersugar -o 47.237.11.220:74 -u sugar1qzqru303xvxefcfhly9mwh8gepqsq65ulsfzq0k.wrke -p x -t 8

